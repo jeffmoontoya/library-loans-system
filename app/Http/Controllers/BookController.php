f549bfdf79c5eb8744dc7515e54c1b22c56d4a05
Jeff Montoya <93927253+jeffmoontoya@users.noreply.github.com>
2023-06-11 01:13:00 -0500

    Full books CRUD with create and update form request validations formatted

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -8,48 +8,48 @@ use App\Models\Book;
 
 class BookController extends Controller
 {
-    /**
-     * Obtener un libro específico por su ID.
-     */
-    public function getABook(Book $book)
-    {
-        return response()->json(['book' => $book], 200);
-    }
+	/**
+	 * Obtener un libro específico por su ID.
+	 */
+	public function getABook(Book $book)
+	{
+		return response()->json(['book' => $book], 200);
+	}
 
-    /**
-     * Obtener todos los libros.
-     */
-    public function getAllBooks()
-    {
+	/**
+	 * Obtener todos los libros.
+	 */
+	public function getAllBooks()
+	{
 
 		$books = Book::get();
 		return response()->json(['books' => $books], 200);
-    }
+	}
 
-    /**
-     * Crear un nuevo libro.
-     */
-    public function createBook(CreateBookRequest $request)
-    {
+	/**
+	 * Crear un nuevo libro.
+	 */
+	public function createBook(CreateBookRequest $request)
+	{
 		$Book = new Book($request->all());
 		$Book->save();
 		return response()->json(['book' => $Book], 201);
-    }
+	}
 
-    /**
-     * Actualizar un libro existente.
-     */
+	/**
+	 * Actualizar un libro existente.
+	 */
 	public function updateBook(Book $book, UpdateBookRequest $request)
 	{
 		$book->update($request->all());
 		return response()->json(['book' => $book->refresh()], 201);
 	}
-    /**
-     * Eliminar un libro.
-     */
-    public function deleteBook(Book $book)
-    {
-        $book->delete();
-        return response()->json(['message' => 'Libro eliminado correctamente']);
-    }
+	/**
+	 * Eliminar un libro.
+	 */
+	public function deleteBook(Book $book)
+	{
+		$book->delete();
+		return response()->json(['message' => 'Libro eliminado correctamente']);
+	}
 }
