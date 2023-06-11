530b4657e5ab3544cbbd9ba32fd04faa8f48f561
Jeff Montoya <93927253+jeffmoontoya@users.noreply.github.com>
2023-06-11 16:09:18 -0500

    Component Example

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,9 +72,11 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <!-- <main class="py-4">
             @yield('content')
-        </main>
+        </main> -->
+
+		<example-component></example-component>
     </div>
 </body>
 </html>
